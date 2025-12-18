dd01af99950d61574660636da3bf5a15b070ac5c
Jaidil14 <jaidilrachmad@gmail.com>
2025-12-18 11:54:55 +0700

    menambahkan data class UIStateSiswa dan DetailSiswa

--- app/src/main/java/com/example/pertemuan12/modeldata/DataSiswa.kt
+++ app/src/main/java/com/example/pertemuan12/modeldata/DataSiswa.kt
@@ -9,3 +9,16 @@ data class DataSiswa(
     val alamat : String,
     val telepon : String
 )
+
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
+
+data class DetailSiswa(
+    val id : Int = 0,
+    val nama : String = "",
+    val alamat : String = "",
+    val telepon : String = ""
+)
+
